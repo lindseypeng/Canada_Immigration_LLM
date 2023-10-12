c8faa6c59e75cf4ef1c0aeb1c61e2cbedc3ac303
lindseypeng <lindsey.peng@mail.utoronto.ca>
2023-10-12 13:56:05 -0400

    revert to initial version

--- chatwithmemory.py
+++ chatwithmemory.py
@@ -16,7 +16,7 @@ import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import sqlite3
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
@@ -24,7 +24,7 @@ with st.sidebar:
 
 
 
-openai.api_key = openai_api_key
+#openai.api_key = openai_api_key
 
 
 ##inference api vs inference endpoint hugging face
@@ -36,8 +36,6 @@ openai.api_key = openai_api_key
 ###openai
 
 
-llm_name= "gpt-3.5-turbo"
-llm = ChatOpenAI(openai_api_key=openai_api_key,model_name = llm_name,temperature=0)
 
 ###hugging face
 
@@ -49,29 +47,7 @@ llm = ChatOpenAI(openai_api_key=openai_api_key,model_name = llm_name,temperature
                      #model_kwargs={"temperature":0.5, "max_new_tokens":250})
 ## load context embedding
 #persist_directory = '/Users/linqianpeng/Documents/immigration/docs/chroma'
-persist_directory = './docs/chroma'
-embedding = OpenAIEmbeddings(openai_api_key=openai.api_key)
-vectordb = Chroma(
-    persist_directory=persist_directory,
-    embedding_function=embedding
-)
-## conversation AI with memory for chat history
-memory = ConversationBufferMemory(
-    memory_key="chat_history",
-    return_messages=True
-)
-retriever=vectordb.as_retriever()
-qa = ConversationalRetrievalChain.from_llm(
-    llm,
-    retriever=retriever,
-    memory=memory)
 
-## conversation AI with no memory, return reference
-qa_chain = RetrievalQA.from_chain_type(
-    llm,
-    retriever=vectordb.as_retriever(),return_source_documents=True,
-    chain_type="stuff"
-)
 
 st.title('question answering canadian immigration')
 
@@ -98,6 +74,31 @@ if prompt := st.chat_input("what is your question?"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 # question= st.text_input('Enter your question here')
+    llm_name= "gpt-3.5-turbo"
+    llm = ChatOpenAI(openai_api_key=openai_api_key,model_name = llm_name,temperature=0)
+    persist_directory = './docs/chroma'
+    embedding = OpenAIEmbeddings(openai_api_key=openai.api_key)
+    vectordb = Chroma(
+        persist_directory=persist_directory,
+        embedding_function=embedding
+    )
+    ## conversation AI with memory for chat history
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        return_messages=True
+    )
+    retriever=vectordb.as_retriever()
+    qa = ConversationalRetrievalChain.from_llm(
+        llm,
+        retriever=retriever,
+        memory=memory)
+
+    ## conversation AI with no memory, return reference
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=vectordb.as_retriever(),return_source_documents=True,
+        chain_type="stuff"
+)
     result = qa_chain({"query": prompt})
     response = result['result']
     # Display assistant response in chat message container
